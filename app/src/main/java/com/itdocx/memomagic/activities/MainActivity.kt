c2b369008a774c8bf13ac2877ef9281ed446b517
ItDocx <107977294+ItDocx@users.noreply.github.com>
2023-05-26 21:41:36 +0500

    RecyclerView Added

--- app/src/main/java/com/itdocx/memomagic/activities/MainActivity.kt
+++ app/src/main/java/com/itdocx/memomagic/activities/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.itdocx.memomagic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+   val REQUEST_CODE_ADD_NOTE =1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val img = findViewById<ImageView>(R.id.iv_addNotes)
+
+        img.setOnClickListener{
+
+            val intent = Intent(this, CreateNotes::class.java)
+            startActivity(intent)
+        }
     }
 }
